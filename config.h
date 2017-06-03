315f7a28738fedf697433a5b0c311b084b815ad5
Krzysztof Witek <kwitek@kw-net.com>
2017-06-03 16:38:19 +0200

    enc28j60: add forgotten file config.h
    Signed-off-by: Krzysztof Witek <kwitek@kw-net.com>

---
+++ config.h
@@ -0,0 +1,14 @@
+#ifndef CONFIG_H
+               # Replace Windows newlines with Unix newlines#define CONFIG_H
+               # Replace Windows newlines with Unix newlines
+               # Replace Windows newlines with Unix newlines#include <avr/io.h>
+               # Replace Windows newlines with Unix newlines
+               # Replace Windows newlines with Unix newlines#define SPI_DDR  DDRB
+               # Replace Windows newlines with Unix newlines#define SPI_PORT PORTB
+               # Replace Windows newlines with Unix newlines#define SPI_CS   2
+               # Replace Windows newlines with Unix newlines#define SPI_MOSI 3
+               # Replace Windows newlines with Unix newlines#define SPI_MISO 4
+               # Replace Windows newlines with Unix newlines#define SPI_SCK  5
+               # Replace Windows newlines with Unix newlines
+               # Replace Windows newlines with Unix newlines#endif
+               # Replace Windows newlines with Unix newlines
