347ed114dcffbf2d1d24fbd7417cb40e741e6d90
Krzysztof Witek <kwitek@kw-net.com>
2018-03-16 15:47:16 +0100

    arch/avr/simu: fix serial config
    Signed-off-by: Krzysztof Witek <kwitek@kw-net.com>

--- arch/avr/simu.c
+++ arch/avr/simu.c
@@ -1,11 +1,11 @@
 /* simulavr/src/simulavr  -f <elf> -d atmega328 -F 16000000 */
 #define SIMU_INFO_FILE src/simulavr_info.h
-#define __incl_header(x) #x
-#define incl_header(x) __incl_header(x)
+#define __str(x) #x
+#define incl_header(x) __str(x)
 #include incl_header(CONFIG_AVR_SIMU_PATH/SIMU_INFO_FILE)
 
-SIMINFO_DEVICE("atmega328");
+SIMINFO_DEVICE(__str(CONFIG_AVR_SIMU_MCU));
 SIMINFO_CPUFREQUENCY(F_CPU);
-SIMINFO_SERIAL_IN("D0", "-", CONFIG_SERIAL_SPEED);
-SIMINFO_SERIAL_OUT("D1", "-", CONFIG_SERIAL_SPEED);
+SIMINFO_SERIAL_IN("D0", "-", CONFIG_USART0_SPEED);
+SIMINFO_SERIAL_OUT("D1", "-", CONFIG_USART0_SPEED);
 
