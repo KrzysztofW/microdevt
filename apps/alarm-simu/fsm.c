9326ee997878bfb61eb257885dade26a4afd1d22
Krzysztof Witek <kwitek@kw-net.com>
2018-07-14 11:47:14 +0200

    apps/alarm-simu: fix simulator
    Signed-off-by: Krzysztof Witek <kwitek@kw-net.com>

---
+++ apps/alarm-simu/fsm.c
@@ -0,0 +1,1 @@
+../alarm/fsm.c
--- apps/alarm-simu/rf-cfg.h
+++ apps/alarm-simu/rf-cfg.h
@@ -1,1 +1,21 @@
-../alarm/rf-cfg.h
+#ifndef _RF_CFG_H_
+#define _RF_CFG_H_
+#include "rf-common.h"
+
+#define RF_SAMPLING_US 150
+#define RF_RING_SIZE   256
+
+/* #define RF_ANALOG_SAMPLING */
+#ifdef RF_ANALOG_SAMPLING
+#define RF_RCV_PIN_NB 0
+#define ANALOG_LOW_VAL 160
+#define ANALOG_HI_VAL  630
+#else
+#define RF_RCV_PIN_NB PC0
+#define RF_RCV_PORT PORTC
+#define RF_RCV_PIN PINC
+#endif
+#define RF_SND_PIN_NB PD1
+#define RF_SND_PORT PORTD
+
+#endif
