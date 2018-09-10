3846ff018ae74496d09bc3721a5f6491d6595240
Krzysztof Witek <kwitek@kw-net.com>
2018-09-10 15:51:42 +0200

    sys/utils: add map() function
    Signed-off-by: Krzysztof Witek <kwitek@kw-net.com>

--- sys/utils.h
+++ sys/utils.h
@@ -36,4 +36,10 @@ static inline uint32_t roundup_pwr2(uint32_t v)
 	return v;
 }
 
+static inline long
+map(long x, long in_min, long in_max, long out_min, long out_max)
+{
+	return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
 #endif
