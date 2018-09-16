1ed7ad866e54cd05927f6a8d3721d6cb9888f21a
Krzysztof Witek <kwitek@kw-net.com>
2018-09-16 18:50:58 +0200

    alarm-module1: use digital input for RF sampling
    The analog ports can also be used as digital inputs thus there is
    no need to use the analog complex and power consuming circuitry.
    
    Signed-off-by: Krzysztof Witek <kwitek@kw-net.com>

--- apps/alarm/alarm-module1/rf-cfg.h
+++ apps/alarm/alarm-module1/rf-cfg.h
@@ -5,14 +5,16 @@
 #define RF_SAMPLING_US 150
 #define RF_RING_SIZE   256
 
-#define RF_ANALOG_SAMPLING
 #ifdef RF_ANALOG_SAMPLING
 #define RF_RCV_PIN_NB 0
 #define ANALOG_LOW_VAL 200
 #define ANALOG_HI_VAL  600
+#else
+#define RF_RCV_PIN_NB PC0
+#define RF_RCV_PORT PORTC
+#define RF_RCV_PIN PINC
 #endif
 
-#define RF_RCV_PIN_NB 0
 #define RF_SND_PIN_NB PD2
 #define RF_SND_PORT PORTD
 
