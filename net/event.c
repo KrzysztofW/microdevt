277a24b76a87c0167450be41f1c10ba7f6232b86
Krzysztof Witek <kwitek@kw-net.com>
2018-09-26 11:26:48 +0200

    net/event: make a difference between proper disconnect and error
    - also always reload the WRITE flag when buffers are available
    
    Signed-off-by: Krzysztof Witek <kwitek@kw-net.com>

--- net/event.c
+++ net/event.c
@@ -10,41 +10,37 @@ static void event_cb(void *arg)
 	while (ev->available && (ev->available & ev->wanted)) {
 		uint8_t events;
 
+		assert(ev->available <= (EV_READ|EV_WRITE|EV_ERROR|EV_HUNGUP));
+		assert(ev->wanted <= (EV_READ|EV_WRITE|EV_ERROR|EV_HUNGUP));
+
 		if (list_empty(ev->rx_queue))
 			ev->available &= ~EV_READ;
 		if (pkt_pool_get_nb_free() == 0) {
 			ev->available &= ~EV_WRITE;
 			if (ev->wanted & EV_WRITE)
 				list_add_tail(&ev->list, &retry_list);
-		}
+		} else
+			ev->available |= EV_WRITE;
 		events = ev->available & ev->wanted;
 		if (events)
 			ev->cb(ev, events);
 	}
 }
 
-static void __event_call(event_t *ev, uint8_t events, uint8_t schedule)
+void event_schedule_event(event_t *ev, uint8_t events)
 {
 	if (events == 0 || ev->cb == NULL)
 		return;
 
 	ev->available |= events;
-	if (ev->wanted & events) {
-		if (schedule)
-			schedule_task(event_cb, ev);
-		else
-			ev->cb(ev, ev->available);
+	if (events & (EV_ERROR | EV_HUNGUP)) {
+		ev->available &= ~EV_WRITE;
+		if (!list_empty(&ev->list))
+			__list_del_entry(&ev->list);
 	}
-}
 
-void event_schedule_event(event_t *ev, uint8_t events)
-{
-	__event_call(ev, events, 1);
-}
-
-void event_call(event_t *ev, uint8_t events)
-{
-	__event_call(ev, events, 0);
+	if (ev->wanted & events)
+		schedule_task(event_cb, ev);
 }
 
 void event_unregister(event_t *ev)
@@ -55,21 +51,12 @@ void event_unregister(event_t *ev)
 		__list_del_entry(&ev->list);
 }
 
-void event_schedule_event_error(event_t *ev)
-{
-	uint8_t events = (ev->available | EV_ERROR) & ~EV_WRITE;
-
-	if (!list_empty(&ev->list))
-		__list_del_entry(&ev->list);
-	event_schedule_event(ev, events);
-}
-
 void event_resume_write_events(void)
 {
 	while (pkt_pool_get_nb_free() && !list_empty(&retry_list)) {
 		event_t *ev = list_first_entry(&retry_list, event_t, list);
 
 		__list_del_entry(&ev->list);
-		__event_call(ev, ev->available | EV_WRITE, 1);
+		event_schedule_event(ev, ev->available | EV_WRITE);
 	}
 }
--- net/event.h
+++ net/event.h
@@ -13,6 +13,7 @@ typedef enum event_flags {
 	EV_READ  = 1,
 	EV_WRITE = 1 << 1,
 	EV_ERROR = 1 << 2,
+	EV_HUNGUP = 1 << 3,
 	/* EV_LAST  = 1 << 7, */
 } event_flags_t;
 
@@ -31,9 +32,15 @@ void event_set(event_t *ev, uint8_t events,
 void event_schedule_event_error(event_t *event);
 void event_resume_write_events(void);
 
+static inline void event_init(event_t *ev)
+{
+	ev->wanted = ev->available = 0;
+	INIT_LIST_HEAD(&ev->list);
+}
+
 static inline void event_set_mask(event_t *ev, uint8_t events)
 {
-	ev->wanted = events | EV_ERROR;
+	ev->wanted = events | EV_ERROR | EV_HUNGUP;
 	if (events && (events & ev->available))
 		event_schedule_event(ev, ev->available);
 }
@@ -42,7 +49,7 @@ static inline void
 event_register(event_t *ev, uint8_t events, list_t *rx_queue,
 	       void (*ev_cb)(event_t *ev, uint8_t events))
 {
-	ev->wanted = events | EV_ERROR;
+	ev->wanted = events | EV_ERROR | EV_HUNGUP;
 	ev->cb = ev_cb;
 	ev->rx_queue = rx_queue;
 }
