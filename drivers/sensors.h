ae018a006055f994a5c4be73f42de4f6471936c0
Krzysztof Witek <kwitek@kw-net.com>
2018-11-05 17:09:37 +0100

    drivers/sensors: support fahrenheit scale for temperature sensors
    Signed-off-by: Krzysztof Witek <kwitek@kw-net.com>

--- drivers/sensors.h
+++ drivers/sensors.h
@@ -7,9 +7,12 @@
 /* tmp36gz temperature sensor */
 #define TMP36GZ_TO_C_DEGREES(millivolts) (((millivolts) - 500) / 10)
 #define TMP36GZ_TO_C_CENTI_DEGREES(millivolts) ((millivolts) - 500)
+#define TMP36GZ_TO_F_DEGREES(millivolts)		\
+	((((millivolts) - 500) * 9) / 50 + 32)
 
 /* LM35dz temperature sensor */
 #define LM35DZ_TO_C_DEGREES(millivolts) ((millivolts) / 10)
 #define LM35DZ_TO_C_CENTI_DEGREES(millivolts) (millivolts)
+#define LM35DZ_TO_F_DEGREES(millivolts) (((millivolts) * 9) / 50 + 32)
 
 #endif
