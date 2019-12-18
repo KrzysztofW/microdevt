432e79bdd3d312b6be1b9a2ea7d2505cc30db3c4
Krzysztof Witek <kwitek@kw-net.com>
2019-12-18 16:26:06 +0100

    apps/hello-world: add basic apps with typical microcontroller usage
    Signed-off-by: Krzysztof Witek <kwitek@kw-net.com>

---
+++ apps/hello-world/gpio.h
@@ -0,0 +1,103 @@
+/*
+ * microdevt - Microcontroller Development Toolkit
+ *
+ * Copyright (c) 2017, Krzysztof Witek
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin St - Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * The full GNU General Public License is included in this distribution in
+ * the file called "LICENSE".
+ *
+ * Contact Information:
+ * Krzysztof Witek <kwitek@kw-net.com>
+*/
+
+#ifndef _GPIO_H_
+#define _GPIO_H_
+#include <adc.h>
+
+/* output pins */
+#define LED PD7
+#define LED_PORT PORTD
+#define LED_DDR  DDRD
+
+#define HUMIDITY_ANALOG_PIN 1
+#define TEMPERATURE_ANALOG_PIN 3
+
+/* input pins */
+#define PIR PD2
+#define PIR_PORT PORTD
+#define PIR_IN_PORT PIND
+#define PIR_DDR DDRD
+
+static inline void pir_int_enable(void)
+{
+	/* PCINT18 enabled (PIR) */
+	PCMSK2 = 1 << PCINT18;
+}
+
+static inline void pir_int_disable(void)
+{
+	/* PCINT18 disabled (PIR) */
+	PCMSK2 = 0;
+}
+
+static inline void gpio_init(void)
+{
+	/* set output pins */
+	LED_DDR |= 1 << LED;
+
+	/* set input pins */
+	PIR_DDR &= ~(1 << PIR);
+
+	/* enable pull-up resistors on unconnected pins */
+	PORTB = 0xFF;
+	PORTD = 0xFF & ~((1 << LED) | (1 << PIR));
+
+	/* analog: PC1 - HIH humidity sensor, PC3 - temperature sensor */
+	PORTC = 0xFF & ~((1 << PC1) | (1 << PC3));
+
+	/* analog input pins */
+	ADC_SET_PRESCALER_64();
+	ADC_SET_REF_VOLTAGE_AVCC();
+	DDRC = 0xFF & ~((1 << PC1) | (1 << PC3));
+
+	pir_int_enable();
+	PCICR = (1 << PCIE2) | (1 << PCIE0);
+}
+
+/* LED */
+static inline void gpio_led_on(void)
+{
+	LED_PORT |= 1 << LED;
+}
+
+static inline void gpio_led_off(void)
+{
+	LED_PORT &= ~(1 << LED);
+}
+
+static inline void gpio_led_toggle(void)
+{
+	LED_PORT ^= 1 << LED;
+}
+
+/* PIR sensor */
+static inline uint8_t gpio_is_pir_on(void)
+{
+	return !!(PIR_IN_PORT & (1 << PIR));
+}
+
+#endif
---
+++ apps/hello-world/main.c
@@ -0,0 +1,197 @@
+/*
+ * microdevt - Microcontroller Development Toolkit
+ *
+ * Copyright (c) 2017, Krzysztof Witek
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin St - Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * The full GNU General Public License is included in this distribution in
+ * the file called "LICENSE".
+ *
+ * Contact Information:
+ * Krzysztof Witek <kwitek@kw-net.com>
+*/
+
+/*
+ * This simple app reads input from the serial console and prints
+ * the relative humidity and temperature.
+ * The humidity and temperature values are read every 10 seconds
+ * and stored in locally.
+ * In case of inactivity, the app goes to sleep (power down mode).
+ * A timer makes a led bink every second and a PIR sensor wakes
+ * the app up (see gpio.h file for PIN assignments).
+
+ * The app shows the usage of the serial console, timers, tasks,
+ * the watchdog, the power management and input/output GPIOs of
+ * an ATMEGA328p microcontroller.
+ */
+
+#include <log.h>
+#include <_stdio.h>
+#include <watchdog.h>
+#include <sys/buf.h>
+#include <sys/ring.h>
+#include <timer.h>
+#include <scheduler.h>
+#include <interrupts.h>
+#include <drivers/sensors.h>
+#include <power-management.h>
+#include "gpio.h"
+
+#define INACTIVITY_TIMEOUT 60 /* enter idle sleep mode after 60 */
+#define ONE_SECOND 1000000
+#define SENSOR_SAMPLE 30 /* sample every 30 seconds */
+static uint16_t sensor_report_elapsed_secs;
+static tim_t one_sec_timer = TIMER_INIT(one_sec_timer);
+static uint16_t humidity_val;
+static uint16_t temp_val;
+
+#define UART_RING_SIZE 16
+STATIC_RING_DECL(uart_ring, UART_RING_SIZE);
+
+static const char *cmd1 = "show sensors";
+
+static void usage(void)
+{
+	LOG("Available commands:\n");
+	LOG(" %s\n\n", cmd1);
+}
+
+static void parse_serial_console(buf_t *buf)
+{
+	sbuf_t s;
+
+	if (buf_get_sbuf_upto_and_skip(buf, &s, cmd1) >= 0) {
+		LOG("humidity: %u%%, temp: %u\n", humidity_val, temp_val);
+		return;
+	}
+	usage();
+}
+
+static void uart_task(void *arg)
+{
+	buf_t buf;
+	int rlen = ring_len(uart_ring);
+
+	if (rlen > UART_RING_SIZE) {
+		ring_reset(uart_ring);
+		return;
+	}
+	buf = BUF(rlen + 1);
+	__ring_get(uart_ring, &buf, rlen);
+	parse_serial_console(&buf);
+	power_management_pwr_down_reset();
+}
+
+ISR(USART_RX_vect)
+{
+	uint8_t c = UDR0;
+
+	if (c == '\r')
+		return;
+	if (c == '\n')
+		schedule_task(uart_task, NULL);
+	else
+		ring_addc(uart_ring, c);
+}
+
+static void pir_interrupt_cb(void *arg)
+{
+	LOG("PIR sensor on\n");
+	power_management_pwr_down_reset();
+}
+
+ISR(PCINT2_vect)
+{
+	schedule_task(pir_interrupt_cb, NULL);
+}
+
+static void sensor_sampling_task(void *arg)
+{
+	adc_enable();
+	humidity_val = adc_read(HUMIDITY_ANALOG_PIN);
+	temp_val = adc_read(TEMPERATURE_ANALOG_PIN);
+
+	humidity_val = hih_4000_to_rh(adc_to_millivolt(humidity_val));
+	temp_val = TMP36GZ_TO_C_CENTI_DEGREES(adc_to_millivolt(temp_val));
+}
+
+static void sensor_sample(void)
+{
+	if (sensor_report_elapsed_secs >= SENSOR_SAMPLE) {
+		sensor_report_elapsed_secs = 0;
+		schedule_task(sensor_sampling_task, NULL);
+	}
+}
+static void timer_one_sec_cb(void *arg)
+{
+	gpio_led_toggle();
+	sensor_report_elapsed_secs++;
+	sensor_sample();
+	timer_reschedule(arg, ONE_SECOND);
+}
+
+#ifdef CONFIG_POWER_MANAGEMENT
+static void watchdog_task(void *arg)
+{
+	/* wait before using the USART to stabilize */
+	delay_ms(100);
+
+	LOG("woke up!\n");
+}
+
+static void watchdog_on_wakeup(void *arg)
+{
+	sensor_report_elapsed_secs += 8 + 2; /* 8 secs of WD + 2 secs below */
+	sensor_sample();
+	schedule_task(watchdog_task, arg);
+
+	/* stay active for 2 seconds (if needed in a project) */
+	power_management_set_inactivity(INACTIVITY_TIMEOUT - 2);
+}
+
+static void pwr_mgr_on_sleep(void *arg)
+{
+	LOG("going to sleep...\n");
+	gpio_led_off();
+
+	/* use the watchdog interrupt to wake the uC up */
+	watchdog_enable_interrupt(watchdog_on_wakeup, arg);
+}
+#endif
+
+int main(void)
+{
+	init_stream0(&stdout, &stdin, 1);
+	LOG("Hello World! version: (%s)\n", VERSION);
+
+	timer_subsystem_init();
+	irq_enable();
+
+#ifdef CONFIG_POWER_MANAGEMENT
+	watchdog_enable(WATCHDOG_TIMEOUT_8S);
+	power_management_power_down_init(INACTIVITY_TIMEOUT, pwr_mgr_on_sleep,
+					 NULL);
+#endif
+	gpio_init();
+	timer_add(&one_sec_timer, ONE_SECOND, timer_one_sec_cb, &one_sec_timer);
+
+	/* interruptible functions */
+	while (1) {
+		scheduler_run_task();
+		watchdog_reset();
+	}
+	return 0;
+}
