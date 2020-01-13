901d5bd3d9421c9d9c6360fe0b93ccadb3c16273
Krzysztof Witek <kwitek@kw-net.com>
2020-01-13 11:34:04 +0100

    arch/x86: fix confusing fake EEPROM functions
    Signed-off-by: Krzysztof Witek <kwitek@kw-net.com>

--- arch/x86/eeprom.h
+++ arch/x86/eeprom.h
@@ -32,13 +32,13 @@ static inline void eeprom_update(void *dst, const void *src, uint8_t size)
 
 static inline void eeprom_load(void *dst, const void *src, uint8_t size)
 {
-	eeprom_update(dst, src, size);
+	memcpy(dst, src, size);
 }
 
 static inline int8_t
 eeprom_update_and_check(void *dst, const void *src, uint8_t size)
 {
-	eeprom_update(dst, src, size);
+	memcpy(dst, src, size);
 	return 0;
 }
 #endif
