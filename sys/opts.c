616b6de9baea2e454b2154551b7635dcbff36f5d
Krzysztof Witek <kwitek@kw-net.com>
2020-01-17 15:10:32 +0100

    sys/opts: size optimizations
    Also add signed arg types.
    
    Signed-off-by: Krzysztof Witek <kwitek@kw-net.com>

--- sys/opts.c
+++ sys/opts.c
@@ -24,6 +24,18 @@
 
 #include "opts.h"
 
+typedef enum loop_ret {
+	LOOP_STOP_FAILED,
+	LOOP_STOP_SUCCESS,
+	LOOP_CONTINUE,
+} loop_ret_t;
+
+typedef struct opts_parse_data {
+	buf_t *in_buf;
+	buf_t *arg_buf;
+	void (*ext_cb)(uint8_t cmd, buf_t *args);
+} opts_parse_data_t;
+
 static int opts_get_arg(uint8_t arg, buf_t *args, buf_t *buf)
 {
 	sbuf_t sbuf;
@@ -31,10 +43,10 @@ static int opts_get_arg(uint8_t arg, buf_t *args, buf_t *buf)
 	buf_t b;
 	char c_end;
 	sbuf_t end;
-	uint16_t v16;
+	int16_t v;
 
 	buf_skip_spaces(buf);
-	if (arg == ARG_TYPE_NONE || buf->len <= 1)
+	if (buf->len <= 1)
 		return -1;
 	if (arg == ARG_TYPE_STRING) {
 		/* take the first string from buf */
@@ -57,86 +69,116 @@ static int opts_get_arg(uint8_t arg, buf_t *args, buf_t *buf)
 	    || buf_addc(&b, c_end) < 0)
 		return -1;
 
-	v16 = atoi((char *)b.data);
+	v = atoi((char *)b.data);
+
+	if (arg < ARG_TYPE_INT8 && v < 0)
+		return -1;
 
 	switch (arg) {
-	case ARG_TYPE_BOOL:
-		if (v16 > 1)
-			return -1;
-		/* fall through */
 	case ARG_TYPE_UINT8:
-		if (v16 > UINT8_MAX)
-			return -1;
-		return buf_addc(args, v16);
+	case ARG_TYPE_INT8:
+		return buf_addc(args, v);
 	case ARG_TYPE_UINT16:
-		return buf_add(args, &v16, sizeof(int16_t));
+	case ARG_TYPE_INT16:
+		return buf_add(args, &v, sizeof(int16_t));
 	default:
 		return -1;
 	}
 	return 0;
 }
 
-int opts_parse_buf(const cmd_t *cmds, uint8_t cmds_len, buf_t *in,
-		   buf_t *args, void (*cb)(uint8_t cmd, buf_t *args))
+static uint8_t opts_parse_buf_cb(const cmd_t *cmd, void *arg)
+{
+	const uint8_t *a;
+	sbuf_t sbuf;
+	opts_parse_data_t *opd = arg;
+
+	if (buf_get_sbuf_upto_sbuf_and_skip(opd->in_buf, &sbuf, cmd->s) < 0)
+		return LOOP_CONTINUE;
+
+	if (opd->in_buf->len > opd->arg_buf->size)
+		return LOOP_STOP_FAILED;
+
+	a = cmd->args;
+	while (a[0] != ARG_TYPE_NONE) {
+		if (opts_get_arg(a[0], opd->arg_buf, opd->in_buf) < 0)
+			return LOOP_STOP_FAILED;
+		a++;
+	}
+	(*opd->ext_cb)(cmd->cmd, opd->arg_buf);
+	return LOOP_STOP_SUCCESS;
+}
+
+static int
+opts_for_each_cmd(const cmd_t *cmds, uint8_t cmds_len,
+		  uint8_t (*cb)(const cmd_t *cmd, void *arg), void *arg)
 {
 	uint8_t i;
 
 	for (i = 0; i < cmds_len; i++) {
 		const cmd_t *cmd = &cmds[i];
-		const uint8_t *a;
-		sbuf_t sbuf;
+		uint8_t ret = (*cb)(cmd, arg);
 
-		if (buf_get_sbuf_upto_sbuf_and_skip(in, &sbuf, cmd->s) < 0)
-			continue;
-		if (in->len > args->size)
+		switch (ret) {
+		case LOOP_STOP_FAILED:
 			return -1;
-
-		a = cmd->args;
-		while (a[0] != ARG_TYPE_NONE) {
-			if (opts_get_arg(a[0], args, in) < 0)
-				return -1;
-			a++;
+		case LOOP_STOP_SUCCESS:
+			return 0;
 		}
-		(*cb)(cmd->cmd, args);
-		return 0;
 	}
 	return -1;
 }
 
-void opts_print_usage(const cmd_t *cmds, uint8_t cmd_len)
+int opts_parse_buf(const cmd_t *cmds, uint8_t cmds_len, buf_t *in,
+		   buf_t *args, void (*cb)(uint8_t cmd, buf_t *args))
+
 {
-	uint8_t i;
+	opts_parse_data_t opd = {
+		.in_buf = in,
+		.arg_buf = args,
+		.ext_cb = cb,
+	};
 
-	for (i = 0; i < cmd_len; i++) {
-		const cmd_t *cmd = &cmds[i];
-		const uint8_t *a = cmd->args;
-		uint8_t j;
+	return opts_for_each_cmd(cmds, cmds_len, opts_parse_buf_cb, &opd);
+}
 
-		LOG("%s", cmd->s->data);
-		for (j = 0; j < countof(cmd->args) &&
-			     *a != ARG_TYPE_NONE; j++) {
-			LOG(" ");
-			switch (*a) {
-			case ARG_TYPE_CHAR:
-				LOG("<char>");
-				break;
-			case ARG_TYPE_BOOL:
-				LOG("<bool>");
-				break;
-			case ARG_TYPE_UINT8:
-				LOG("<uint8>");
-				break;
-			case ARG_TYPE_UINT16:
-				LOG("<uint16>");
-				break;
-			case ARG_TYPE_STRING:
-				LOG("<string>");
-				break;
-			default:
-				__abort();
-			}
-			a++;
+static uint8_t opts_print_usage_cb(const cmd_t *cmd, void *args)
+{
+	const uint8_t *a = cmd->args;
+	uint8_t j;
+
+	LOG("%s", cmd->s->data);
+	for (j = 0; j < countof(cmd->args) && *a != ARG_TYPE_NONE; j++) {
+		switch (*a) {
+		case ARG_TYPE_CHAR:
+			LOG(" <char>");
+			break;
+		case ARG_TYPE_UINT8:
+			LOG(" <uint8>");
+			break;
+		case ARG_TYPE_UINT16:
+			LOG(" <uint16>");
+			break;
+		case ARG_TYPE_INT8:
+			LOG(" <int8>");
+			break;
+		case ARG_TYPE_INT16:
+			LOG(" <int16>");
+			break;
+		case ARG_TYPE_STRING:
+			LOG(" <string>");
+			break;
+		default:
+			goto end;
 		}
-		LOG("\n");
+		a++;
 	}
+ end:
+	LOG("\n");
+	return LOOP_CONTINUE;
+}
+
+void opts_print_usage(const cmd_t *cmds, uint8_t cmds_len)
+{
+	opts_for_each_cmd(cmds, cmds_len, opts_print_usage_cb, NULL);
 }
