38d50212dc85042f7bac9c7ac43f0464cce74968
Krzysztof Witek <kwitek@kw-net.com>
2024-07-10 21:49:46 +0200

    apps/kbd-usb: add VirtualSerial device
    Signed-off-by: Krzysztof Witek <kwitek@kw-net.com>

--- apps/kbd-usb/Descriptors.h
+++ apps/kbd-usb/Descriptors.h
@@ -46,11 +46,23 @@
  *  application code, as the configuration descriptor contains several sub-descriptors which
  *  vary between devices, and which describe the device's usage to the host.
  */
-typedef struct
-{
+typedef struct {
 	USB_Descriptor_Configuration_Header_t Config;
 
-	// Keyboard HID Interface
+	/* CDC Control Interface */
+	USB_Descriptor_Interface_Association_t   CDC_IAD;
+	USB_Descriptor_Interface_t               CDC_CCI_Interface;
+	USB_CDC_Descriptor_FunctionalHeader_t    CDC_Functional_Header;
+	USB_CDC_Descriptor_FunctionalACM_t       CDC_Functional_ACM;
+	USB_CDC_Descriptor_FunctionalUnion_t     CDC_Functional_Union;
+	USB_Descriptor_Endpoint_t                CDC_NotificationEndpoint;
+
+	/* CDC Data Interface */
+	USB_Descriptor_Interface_t               CDC_DCI_Interface;
+	USB_Descriptor_Endpoint_t                CDC_DataOutEndpoint;
+	USB_Descriptor_Endpoint_t                CDC_DataInEndpoint;
+
+	/* Keyboard HID Interface */
 	USB_Descriptor_Interface_t            HID_Interface;
 	USB_HID_Descriptor_HID_t              HID_KeyboardHID;
 	USB_Descriptor_Endpoint_t             HID_ReportINEndpoint;
@@ -60,23 +72,37 @@ typedef struct
  *  should have a unique ID index associated with it, which can be used to refer to the
  *  interface from other descriptors.
  */
-enum InterfaceDescriptors_t
-	{
-		INTERFACE_ID_Keyboard = 0, /**< Keyboard interface descriptor ID */
-	};
+enum InterfaceDescriptors_t {
+	INTERFACE_ID_CDC_CCI = 0, /**< CDC CCI interface descriptor ID */
+	INTERFACE_ID_CDC_DCI = 1, /**< CDC DCI interface descriptor ID */
+	INTERFACE_ID_Keyboard = 2, /**< Keyboard interface descriptor ID */
+};
 
 /** Enum for the device string descriptor IDs within the device. Each string descriptor should
  *  have a unique ID index associated with it, which can be used to refer to the string from
  *  other descriptors.
  */
-enum StringDescriptors_t
-	{
-		STRING_ID_Language     = 0, /**< Supported Languages string descriptor ID (must be zero) */
-		STRING_ID_Manufacturer = 1, /**< Manufacturer string ID */
-		STRING_ID_Product      = 2, /**< Product string ID */
-	};
+enum StringDescriptors_t {
+	STRING_ID_Language     = 0, /**< Supported Languages string descriptor ID (must be zero) */
+	STRING_ID_Manufacturer = 1, /**< Manufacturer string ID */
+	STRING_ID_Product      = 2, /**< Product string ID */
+};
 
-/* Macros: */
+/** Endpoint address of the CDC device-to-host notification IN endpoint. */
+#define CDC_NOTIFICATION_EPADDR        (ENDPOINT_DIR_IN  | 2)
+
+/** Endpoint address of the CDC device-to-host data IN endpoint. */
+#define CDC_TX_EPADDR                  (ENDPOINT_DIR_IN  | 3)
+
+/** Endpoint address of the CDC host-to-device data OUT endpoint. */
+#define CDC_RX_EPADDR                  (ENDPOINT_DIR_OUT | 4)
+
+/** Size in bytes of the CDC device-to-host notification IN endpoint. */
+#define CDC_NOTIFICATION_EPSIZE        8
+
+/** Size in bytes of the CDC data IN and OUT endpoints. */
+#define CDC_TXRX_EPSIZE                16
+
 /** Endpoint address of the Keyboard HID reporting IN endpoint. */
 #define KEYBOARD_EPADDR              (ENDPOINT_DIR_IN | 1)
 
